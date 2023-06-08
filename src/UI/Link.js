f81746261b3cb97718480ad2f08fab41e1f82906
David Emmanuel <david3emmanuel@gmail.com>
2023-06-08 13:31:41 +0100

    Changed avatar, added navlink

--- src/UI/Link.js
+++ src/UI/Link.js
@@ -1,9 +1,10 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
-export default function _Link({to, title, className, children}) {
+export default function _Link({to, title, className, children, nav}) {
     if (!to || to.match(/.+\..+/)) {
         return <a rel="noreferrer" target="_blank" className={className} title={title} href={to || '/'}>{children}</a>
     }  else {
-        return <Link className={className} title={title} to={to}>{children}</Link>
+        if (nav) return <NavLink className={className} title={title} to={to}>{children}</NavLink>
+        else return <Link className={className} title={title} to={to}>{children}</Link>
     }
 }
--- src/shared/Header.js
+++ src/shared/Header.js
@@ -14,16 +14,16 @@ export default function Header() {
     return <>
         <header>
             <Card className="center avatar">
-                <img src="/avatar.png" alt="Me" />
+                <img src="/avatar.jpg" alt="Me" />
                 <h2 className="header-text">David Emmanuel</h2>
             </Card>
         </header>
         <nav>
             <ul>
-                <li><Link to='/'><h3>HOME</h3></Link></li>
-                <li><Link to='/projects'><h3>PROJECTS</h3></Link></li>
-                <li><Link to='/blog'><h3>BLOG</h3></Link></li>
-                {/* <li><Link><h3>LOG IN</h3></Link></li> */}
+                <li><Link to='/' nav><h3>HOME</h3></Link></li>
+                <li><Link to='/projects' nav><h3>PROJECTS</h3></Link></li>
+                <li><Link to='/blog' nav><h3>BLOG</h3></Link></li>
+                <li><Link to='/login'><h3>LOG IN</h3></Link></li>
                 <li><button className="clean" onClick={() => setTheme(prev => !prev)}><span className="material-icons">brightness_4</span></button></li>
             </ul>
         </nav>
