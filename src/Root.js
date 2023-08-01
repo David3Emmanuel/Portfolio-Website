1db43d98a5932cba15826ed85026c61ed8490f44
David Emmanuel <david3emmanuel@gmail.com>
2023-08-01 01:43:23 +0100

    Added footer

--- src/Root.js
+++ src/Root.js
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./shared/Header";
+import Footer from "./shared/Footer";
 
 export default function Root({children}) {
     return <div className="root">
@@ -7,6 +8,6 @@ export default function Root({children}) {
         <main>
             { children || <Outlet /> }
         </main>
-        <footer></footer>
+        <Footer />
     </div>
 }
---
+++ src/shared/Footer.js
@@ -0,0 +1,23 @@
+import Link from "../UI/Link";
+
+import "./Footer.css";
+
+export default function Footer() {
+    return <footer>
+        <section>
+            <h3><Link to="/">Home Page</Link></h3>
+            <h3><Link to="/projects">Projects</Link></h3>
+            <h3><Link to="/blog">Blog Posts</Link></h3>
+        </section>
+        <section>
+            <h3>Contact Me:</h3>
+            <ul>
+                <p><img src="/gmail.ico" alt="Email" /><Link to="mailto: david3emmanuel@gmail.com">david3emmanuel@gmail.com</Link></p>
+                <p><img src="/github2.ico" alt="Github" /><Link to="https://github.com/David3Emmanuel">GitHub</Link></p>
+                <p><img src="/whatsapp.ico" alt="Whatsapp" /><Link to="https://wa.me/2347012117811">Phone: +234 7012117811</Link></p>
+                <p><img src="/linkedin.ico" alt="Linkedin" /><Link to="https://linkedin.com/David3Emmanuel">LinkedIn</Link></p>
+                <p><img src="/twitter.ico" alt="Twitter" /><Link to="https://twitter.com/David3Emmanuel">Twitter</Link></p>
+            </ul>
+        </section>
+    </footer>
+}
